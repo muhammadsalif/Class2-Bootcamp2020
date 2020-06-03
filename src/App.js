fa83554020aefc9f2434550979581e9b60fb78e3
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-03 15:02:36 +0500

    updating file.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Updating As Salif.</p>
+        <p>Updating File Salif.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
